122811bb6624a68ee57c57c5a9956174ee7a4e7c
rubenrudov <ruby.rudov@gmail.com>
2021-06-29 01:38:44 +0300

    Adding TTS answer from the computer's speaker

--- VoiceActions/Commands.py
+++ VoiceActions/Commands.py
@@ -6,21 +6,39 @@ Purpose: Basic speech recognition program for using in more advanced tasks later
 
 import speech_recognition as sr
 from datetime import datetime
+from gtts import gTTS
+import os
+import playsound
+
 RECOGNIZER = sr.Recognizer()
+
 
 # TODO: Add TTS for program responding
 
 
+def say(text, FILE_COUNT):
+    """
+    :param FILE_COUNT:
+    :param text: str that contains the proffered voice output
+    :return: None
+    """
+    text_to_speech = gTTS(text=text, lang='en')
+    text_to_speech.save(f'audio{FILE_COUNT}.mp3')
+    playsound.playsound(f'audio{FILE_COUNT}.mp3')
+    print(os.remove(f'audio{FILE_COUNT}.mp3'))
+
+
 class CommandListener:
-    def __init__(self):
+    def __init__(self, count):
         self.recognizer = RECOGNIZER
         self.command = "DEFAULT"
         self.tts_mute = False
+        self.file_count = count
 
     def basic_commands_listener(self):
         """
         :purpose: Do easy commands by voice commanding and TTS answer
-        :return:
+        :return: Boolean if function should run or not
         """
         with sr.Microphone() as source:
             print("Listening for commands")
@@ -28,14 +46,17 @@ class CommandListener:
             audio = self.recognizer.listen(source)
 
             try:
-                print("Recognizing command")
+                print("Recognizing command...")
                 query = self.recognizer.recognize_google(audio)
 
                 if query == "time":
-                    print(datetime.now())
+                    say(datetime.strftime(datetime.now(), "%H:%M:%S"), self.get_count())
+                    print(str(datetime.now()))
 
-                elif query == "turn off":
-                    print("Not gonna happen...")
+                elif query == "stop":
+                    # For turning of the listening system
+                    say("Not gonna happen...", self.get_count())
+                    return False
 
                 elif query == "mute responses":
                     self.tts_mute = True
@@ -43,11 +64,19 @@ class CommandListener:
                 elif str(query).replace(" ", "") == "unmuteresponse":
                     self.tts_mute = False
 
+                # TODO: Add more voice commands for the system
+
                 else:
                     print("Command not in the commands list")
+                    return False
 
             except sr.UnknownValueError:
                 print("Could not understand the command")
 
             print(f"Debug: {query.lower()}")
 
+        return True
+
+    def get_count(self):
+        # Accessor for audio files count
+        return self.file_count
--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+"""
+Author: Ruben Rudov
+Date: 29/06/2021
+Purpose: Call and handle all the required objects
+"""
+
 import VoiceActions.Commands as VoiceCommands
 
 
 def main():
-    command_listener = VoiceCommands.CommandListener()
-    command_listener.basic_commands_listener()  # Check if works
+    run_listener = True
+    file_count = 0
+
+    while run_listener:
+        command_listener = VoiceCommands.CommandListener(file_count)
+        run_listener = command_listener.basic_commands_listener()  # Check if works
+        file_count += 1  # Increments the file number for this program
 
 
 if __name__ == '__main__':
